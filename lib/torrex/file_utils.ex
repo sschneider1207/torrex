cf1a3ce544484a6538afc65b02aaf83bdd836b87
Sam Schneider <sds6065@gmail.com>
2016-12-28 22:25:19 -0500

    fix torrent hashing for multiple files: :

--- lib/torrex/file_utils.ex
+++ lib/torrex/file_utils.ex
@@ -9,19 +9,74 @@ defmodule Torrex.FileUtils do
   """
   @spec hash_pieces(String.t | [String.t], non_neg_integer) :: <<_::_ * 20>>
   def hash_pieces(paths, piece_length) when is_list(paths) do
-    paths
-    |> Enum.with_index()
-    |> Flow.from_enumerable()
-    |> Flow.map(fn {path, index} -> {hash_pieces(path, piece_length), index} end)
-    |> Enum.to_list()
-    |> Enum.sort_by(fn {_, index} -> index end)
-    |> Enum.reduce(<<>>, fn {piece, _index}, acc -> acc <> piece end)
+    do_hash_pieces(paths, piece_length)
+    #paths
+    #|> Enum.with_index()
+    #|> Flow.from_enumerable()
+    #|> Flow.map(fn {path, index} -> {hash_pieces(path, piece_length), index} end)
+    #|> Enum.to_list()
+    #|> Enum.sort_by(fn {_, index} -> index end)
+    #|> Enum.reduce(<<>>, fn {piece, _index}, acc -> acc <> piece end)
   end
   def hash_pieces(path, piece_length) do
     path
     |> File.stream!([], piece_length)
     |> Stream.map(&:crypto.hash(:sha, &1))
     |> Enum.reduce(<<>>, &Kernel.<>(&2, &1))
+  end
+
+  defp do_hash_pieces(paths, piece_length, acc \\ <<>>)
+  defp do_hash_pieces([], _piece_length, final), do: final
+  defp do_hash_pieces([path|rest], piece_length, acc) do
+    size = :filelib.file_size(path)
+    {:ok, file} = :file.open(path, [:read, :raw, :binary])
+    case do_hash_piece(file, piece_length, size, acc) do
+      {:done, final} ->
+        :file.close(file)
+        do_hash_pieces(rest, piece_length, final)
+      {:rem, final, partial_piece, rem_bytes} ->
+        :file.close(file)
+        do_hash_pieces_rem(rest, piece_length, final, partial_piece, rem_bytes)
+    end
+  end
+
+  def do_hash_pieces_rem([], _piece_length, acc, partial_piece, _rem_bytes) do
+    acc <> :crypto.hash(:sha, partial_piece)
+  end
+  def do_hash_pieces_rem([path|rest], piece_length, acc, partial_piece, rem_bytes) do
+    size = :filelib.file_size(path)
+    {:ok, file} = :file.open(path, [:read, :raw, :binary])
+    cond do
+      size < rem_bytes ->
+        {:ok, partial_bytes} = :file.read(file, size)
+        :file.close(file)
+        do_hash_pieces_rem(rest, piece_length, acc, partial_piece <> partial_bytes, rem_bytes - size)
+      true ->
+        {:ok, other_partial_piece} = :file.read(file, rem_bytes)
+        acc = acc <> :crypto.hash(:sha, partial_piece <> other_partial_piece)
+        case do_hash_piece(file, piece_length, size, acc, rem_bytes) do
+          {:done, final} ->
+            :file.close(file)
+            do_hash_pieces(rest, piece_length, final)
+          {:rem, final, partial_piece, rem_bytes} ->
+            :file.close(file)
+            do_hash_pieces_rem(rest, piece_length, final, partial_piece, rem_bytes)
+        end
+    end
+  end
+
+  defp do_hash_piece(file, piece_length, total, acc, read \\ 0)
+  defp do_hash_piece(_file, _piece_length, total, final, total), do: {:done, final}
+  defp do_hash_piece(file, piece_length, total, acc, read) when read + piece_length <= total  do
+    {:ok, iodata} = :file.read(file, piece_length)
+    bytes = :erlang.iolist_to_binary(iodata)
+    acc = acc <> :crypto.hash(:sha, bytes)
+    do_hash_piece(file, piece_length, total, acc, read + piece_length)
+  end
+  defp do_hash_piece(file, piece_length, total, final, read) do
+    rem = total - read
+    {:ok, bytes} = :file.read(file, rem)
+    {:rem, final, bytes, piece_length - rem}
   end
 
   @doc """
